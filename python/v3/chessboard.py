ef2c6a6244a8094516e8ce0734b67fd60163634b
Vivek Kwatra <kwatra@kwatra-macbookpro2.roam.corp.google.com>
2020-02-06 10:25:22 -0800

    Modularize for use with tracker.

--- python/v3/chessboard.py
+++ python/v3/chessboard.py
@@ -6,11 +6,11 @@ import time
 
 
 class CameraSource(object):
-  def __init__(self, camera_source, output_file=None):
+  def __init__(self, camera_source, height, output_file=None):
     self.camera = cv2.VideoCapture(camera_source)
     self.ORIGINAL_WIDTH = int(self.camera.get(cv2.CAP_PROP_FRAME_WIDTH))
     self.ORIGINAL_HEIGHT = int(self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    self.HEIGHT = min(self.ORIGINAL_HEIGHT, 360)
+    self.HEIGHT = min(self.ORIGINAL_HEIGHT, height)
     self.WIDTH = int(self.ORIGINAL_WIDTH * self.HEIGHT / self.ORIGINAL_HEIGHT)
     self.startTime = time.time()
     self.nFrames = 0
@@ -24,6 +24,7 @@ class CameraSource(object):
         self.writer = None
     print(self.ORIGINAL_WIDTH)
 
+
   def GetFrame(self):  
     frame = None
     if self.camera.isOpened():
@@ -36,8 +37,10 @@ class CameraSource(object):
         frame = cv2.resize(frame, (self.WIDTH, self.HEIGHT))
     return frame
 
+
   def ImageSize(self):
     return (self.WIDTH, self.HEIGHT)
+
 
   def OutputFrameAndTestContinue(self, message, frame):
     if self.writer:
@@ -46,12 +49,14 @@ class CameraSource(object):
     k = cv2.waitKey(1) & 0xFF
     return k != 27
 
+
   def __del__(self):
     if self.writer:
       self.writer.release()
     self.camera.release()
     cv2.destroyAllWindows()
         
+
 
 class Chessboard(object):
   def __init__(self, rows=6, cols=8, squareWidth=8.0):
@@ -60,6 +65,7 @@ class Chessboard(object):
         np.mgrid[0:cols, 0:rows].T.reshape(-1,2) * squareWidth)
     self.patternSize = (cols, rows)
     self.squareWidth = squareWidth
+
 
   def GetObjectAndImagePoints(self, frame, draw=True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -73,100 +79,146 @@ class Chessboard(object):
     else:
       return None, None
 
+
   def SquareWidth(self):
     return self.squareWidth
 
 
-def RunCalibration(calibVideo, maxSamples=0, forceRecompute=False):
-  nChessFrames = 0
-  objectPoints = []
-  imagePoints = []
 
-  camera = CameraSource(calibVideo)
-  chess = Chessboard()
+class Calibration(object):
+  def __init__(self, calibVideo, imageHeight, maxSamples):
+    name = str(imageHeight) + '-' + str(maxSamples)
+    self.calibFileCam = calibVideo + '-' + str(name) + '-calib_cam.txt'
+    self.calibFileDist = calibVideo + '-' + str(name) + '-calib_dist.txt'
+    self.imageHeight = imageHeight
+    self.calibVideo = calibVideo
+    self.maxSamples = maxSamples
+    self.hasCalib = False
+    self.cameraMatrix = None
+    self.distCoeffs = None
 
-  height = camera.ImageSize()[1]
-  id = str(height) + '-' + str(maxSamples)
-  calibFileCam = calibVideo + '-' + str(id) + '-calib_cam.txt'
-  calibFileDist = calibVideo + '-' + str(id) + '-calib_dist.txt'
+
+  def PrintInfo(self):
+    print('Calibration files:')
+    print(self.calibFileCam)
+    print(self.calibFileDist)
+    if self.hasCalib:
+      print('CameraMatrix:\n', self.cameraMatrix)
+      print('DistCoeffs:\n', self.distCoeffs)
+    else:
+      print('Calibration not loaded or computed.')
+
+
+  def LoadFromFile(self):
+    if self.hasCalib:
+      return True
     
-  print('Calibration files:')
-  print(calibFileCam)
-  print(calibFileDist)
+    # Read from file.
+    self.hasCalib = False
+    if (os.path.exists(self.calibFileCam) and
+        os.path.exists(self.calibFileDist)):
+      print('Loading calibratin info from files.')
+      self.cameraMatrix = np.loadtxt(self.calibFileCam)
+      success = self.cameraMatrix.shape == (3, 3)
+      if success:
+        self.distCoeffs = np.loadtxt(self.calibFileDist)
+        success = self.distCoeffs.shape == (5,)
+        if success:
+          self.hasCalib = True
+          print('Loaded successfully.')
+          self.PrintInfo()
+          return True
 
-  if forceRecompute:
-    print('Forcing recomputation of calibration data.')
+    # Failed to read.
+    if not self.hasCalib:
+      print('Failed to load calibration from files.')
+      self.cameraMatrix = None
+      self.distCoeffs = None
+      return False
+
+
+  def LoadOrCompute(self, forceRecompute=False):
+    if forceRecompute:
+      print('Forcing recomputation of calibration data.')
+    elif self.LoadFromFile():
+      return True
+
+    nChessFrames = 0
+    objectPoints = []
+    imagePoints = []
  
-  loadedCalib = False
-  if (not forceRecompute and os.path.exists(calibFileCam) and
-        os.path.exists(calibFileDist)):
-    print('Loading calibratin info from files.')
-    cameraMatrix = np.loadtxt(calibFileCam)
-    success = cameraMatrix.shape == (3, 3)
-    if success:
-      distCoeffs = np.loadtxt(calibFileDist)
-      success = distCoeffs.shape == (5,)
-    if success:
-      loadedCalib = True
-      print('Loaded successfully.')
-      print('CameraMatrix:\n', cameraMatrix)
-      print('DistCoeffs:\n', distCoeffs)
-      return cameraMatrix, distCoeffs
-    print('Failed to load calibration from files.')
-
-  print('Extracting frames for calibration.')
-  while True:
-    frame = camera.GetFrame()
-    if frame is None:
-      break
-    if maxSamples > 0 and len(imagePoints) >= maxSamples:
-      break
-
-    calibSample = False
-    objectPointsFrame, imagePointsFrame = chess.GetObjectAndImagePoints(
-        frame, draw=True)
-    if objectPointsFrame is not None:
-      nChessFrames += 1
-      if nChessFrames % 100 == 0:
-        calibSample = True
-        objectPoints.append(objectPointsFrame)
-        imagePoints.append(imagePointsFrame)
-        cv2.putText(frame, 'CALIBRATION SAMPLE', (50, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-        print('Extracted calibration sample ', len(imagePoints))
-           
-    # Display frame.
-    if not camera.OutputFrameAndTestContinue('chess', frame):
-      break
-    if calibSample:
-      time.sleep(0.5)
-  
-  print('Calibrating...')
-  ret, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(
-      objectPoints, imagePoints, camera.ImageSize(), None, None) 
-  
-  print('Saving calibratin file ', calibFileCam)
-  print(cameraMatrix)
-  np.savetxt(calibFileCam, cameraMatrix)
-  print('Saving calibratin file ', calibFileDist)
-  print(distCoeffs)
-  np.savetxt(calibFileDist, distCoeffs)
-  print('Done.')
-
-  return cameraMatrix, distCoeffs
+    camera = CameraSource(self.calibVideo, self.imageHeight)
+    chess = Chessboard()
+ 
+    print('Extracting frames for calibration.')
+    while True:
+      frame = camera.GetFrame()
+      if frame is None:
+        break
+      if self.maxSamples > 0 and len(imagePoints) >= self.maxSamples:
+        break
+ 
+      calibSample = False
+      objectPointsFrame, imagePointsFrame = chess.GetObjectAndImagePoints(
+          frame, draw=True)
+      if objectPointsFrame is not None:
+        nChessFrames += 1
+        if nChessFrames % 100 == 0:
+          calibSample = True
+          objectPoints.append(objectPointsFrame)
+          imagePoints.append(imagePointsFrame)
+          cv2.putText(frame, 'CALIBRATION SAMPLE', (50, 50),
+                      cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+          print('Extracted calibration sample ', len(imagePoints))
+             
+      # Display frame.
+      if not camera.OutputFrameAndTestContinue('chess', frame):
+        break
+      if calibSample:
+        time.sleep(0.5)
+    
+    print('Calibrating...')
+    _, self.cameraMatrix, self.distCoeffs, _, _ = cv2.calibrateCamera(
+        objectPoints, imagePoints, camera.ImageSize(), None, None) 
+    self.hasCalib = True
+    
+    self.PrintInfo()
+    print('Saving camera matrix to: ', self.calibFileCam)
+    print('Saving distortion coeffs to: ', self.calibFileDist)
+    np.savetxt(self.calibFileCam, self.cameraMatrix)
+    np.savetxt(self.calibFileDist, self.distCoeffs)
+    print('Done.')
+    return True
 
 
-def RunPoseEstimation(video, cameraMatrix, distCoeffs):
-  output_file = video + '-detected_pose.mp4'
-  camera = CameraSource(video, output_file)
+class CoordinateFrame(object):
+  def __init__(self, axisLength):
+    self.coordFrame = np.zeros((4, 3), np.float32)
+    self.coordFrame[0, ...] = [0, 0, 0]
+    self.coordFrame[1, ...] = [1, 0, 0]
+    self.coordFrame[2, ...] = [0, 1, 0]
+    self.coordFrame[3, ...] = [0, 0, -1]
+    self.coordFrame *= axisLength
+    
+
+  def Draw(self, frame, rvec, tvec, calib):
+    coords, _ = cv2.projectPoints(
+        self.coordFrame, rvec, tvec, calib.cameraMatrix, calib.distCoeffs)
+    # Draw coordinate axes.
+    coords = [tuple(np.squeeze(x).astype(int)) for x in coords.tolist()]
+    cv2.arrowedLine(frame, coords[0], coords[1], (  0, 255, 0), 2)
+    cv2.arrowedLine(frame, coords[0], coords[2], (255, 0, 255), 2)
+    cv2.arrowedLine(frame, coords[0], coords[3], (0, 255, 255), 2)
+
+
+
+def RunPoseEstimation(video, calib):
+  calib_name = str(calib.imageHeight) + '-' + str(calib.maxSamples)
+  output_file = video + '-' + calib_name + '-detected_pose.mp4'
+  camera = CameraSource(video, calib.imageHeight, output_file)
   chess = Chessboard()
   
-  coordFrame = np.zeros((4, 3), np.float32)
-  coordFrame[0, ...] = [0, 0, 0]
-  coordFrame[1, ...] = [1, 0, 0]
-  coordFrame[2, ...] = [0, 1, 0]
-  coordFrame[3, ...] = [0, 0, -1]
-  coordFrame *= chess.SquareWidth() * 2
+  coordFrame = CoordinateFrame(chess.SquareWidth() * 2)
 
   rvec = None
   tvec = None
@@ -179,32 +231,34 @@ def RunPoseEstimation(video, cameraMatrix, distCoeffs):
     objectPoints, imagePoints = chess.GetObjectAndImagePoints(frame, draw=False)
     if objectPoints is not None:
       _, rvec, tvec = cv2.solvePnP(
-          objectPoints, imagePoints, cameraMatrix, distCoeffs,
+          objectPoints, imagePoints, calib.cameraMatrix, calib.distCoeffs,
           rvec=rvec, tvec=tvec, useExtrinsicGuess=useExtrinsicGuess)
-      # Combine checkerboard points and coordinate axes points.
-      objectPoints = np.concatenate((objectPoints, coordFrame), 0)
       useExtrinsicGuess = True
-      projectedPoints, _ = cv2.projectPoints(
-          objectPoints, rvec, tvec, cameraMatrix, distCoeffs)
+
       # Draw checkerboard points.
-      checkerboard = projectedPoints[0:-4]
+      checkerboard, _ = cv2.projectPoints(
+          objectPoints, rvec, tvec, calib.cameraMatrix, calib.distCoeffs)
       for point in checkerboard:
         point = np.squeeze(point)
         cv2.circle(frame, tuple(point), 4, (255, 0, 0), 2)
+
       # Draw coordinate axes.
-      coords = projectedPoints[-4:]
-      coords = [tuple(np.squeeze(x).astype(int)) for x in coords.tolist()]
-      cv2.arrowedLine(frame, coords[0], coords[1], (  0, 255, 0), 2)
-      cv2.arrowedLine(frame, coords[0], coords[2], (255, 0, 255), 2)
-      cv2.arrowedLine(frame, coords[0], coords[3], (0, 255, 255), 2)
+      coordFrame.Draw(frame, rvec, tvec, calib)
+
       if not camera.OutputFrameAndTestContinue('SolvePnP', frame):
         break
 
-
 def main():
   calibVideo = '/Users/kwatra/Home/pvt/robotx/RobotX2020VisionSystem/data/chessboard-tv.mp4'
-  cameraMatrix, distCoeffs = RunCalibration(calibVideo, maxSamples=25)
-  RunPoseEstimation(calibVideo, cameraMatrix, distCoeffs)
+  imageHeight = 360
+  maxSamples = 25
+  
+  calib = Calibration(calibVideo, imageHeight, maxSamples) 
+  if not calib.LoadOrCompute():
+    print('Could not load or compute calibration.')
+    return
+
+  RunPoseEstimation(calibVideo, calib) 
 
 
 if __name__ == '__main__':
