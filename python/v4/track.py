da0a9dc03de5e7422cf5309c3b229c3083208c08
Abhinav Vedati <52864339+Avedati@users.noreply.github.com>
2020-02-29 11:05:34 -0800

    Update track.py

--- python/v4/track.py
+++ python/v4/track.py
@@ -7,6 +7,31 @@ import time
 #from tests import *
 from networktables import NetworkTables
 
+"""
+import threading
+from networktables import NetworkTables
+
+cond = threading.Condition()
+notified = [False]
+
+def connectionListener(connected, info):
+    print(info, '; Connected=%s' % connected)
+    with cond:
+        notified[0] = True
+        cond.notify()
+
+NetworkTables.initialize(server='10.xx.xx.2')
+NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+
+with cond:
+    print("Waiting")
+    if not notified[0]:
+        cond.wait()
+
+# Insert your processing code here
+print("Connected!")
+"""
+
 sd = NetworkTables.getTable('SmartDashboard')
 
 # Assume indexed contour for all functions here.
